62a4a9e57bd2fa6b1330f30dc0d7e4001f549545
Bogdan <bodya.tretyak9898@gmail.com>
2019-11-02 15:14:37 +0200

    asd

--- KavresBot/Program.cs
+++ KavresBot/Program.cs
@@ -7,14 +7,24 @@ using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InlineQueryResults;
 using Telegram.Bot.Types.ReplyMarkups;
+using TelegramEmoji;
+using FirebaseAdmin;
 
 
 namespace KavresBot
 {
     class Program
     {
+        private static volatile List<UserInfo> users = new List<UserInfo>();
         private static TelegramBotClient bot;
         private const string tKey = "767958766:AAEyyBPHyzO_5HnwYF4wswJIP3Dn_PKmd1A";
+
+        private static string oftenAnswer1 = "1 " + Emojis.Smileys_And_People.Body.Emotion.beating_heart;
+        private static string oftenAnswer2 = "2 " + Emojis.Smileys_And_People.Body.Emotion.growing_heart;
+        private static string oftenAnswer3 = ">2 " + Emojis.Smileys_And_People.Body.Emotion.red_heart;
+
+        private static string restAnswer1 = "Зимний " + Emojis.Travel_And_Places.Sky_And_Weather.cloud;
+        private static string restAnswer2 = "Летний " + Emojis.Travel_And_Places.Sky_And_Weather.sun;
 
         static void Main(string[] args)
         {
@@ -30,17 +40,7 @@ namespace KavresBot
 
         private async static void CallbackHandler(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
         {
-            switch (e.CallbackQuery.Data)
-            {
-                case "@kavres":
-                    await bot.AnswerCallbackQueryAsync(e.CallbackQuery.Id, "You contant to Dmitriy");
-                    break;
-                case "@tretyakoff_b":
-                    await bot.AnswerCallbackQueryAsync(e.CallbackQuery.Id, "You contant to Bogdan");
-                    break;
-                default:
-                    break;
-            }
+
         }
 
         private static async void MessageHandler(object sender, Telegram.Bot.Args.MessageEventArgs e)
@@ -52,11 +52,38 @@ namespace KavresBot
             {
                 if (message.Text == "/start")
                 {
-                    List<InlineKeyboardButton> buttons = new List<InlineKeyboardButton>();
-                    buttons.Add(InlineKeyboardButton.WithUrl("@kavres", "https://t.me/kavres"));
-                    buttons.Add(InlineKeyboardButton.WithUrl("@tretyakoff_b", "https://t.me/tretyakoff_b"));
-                    var inline = new InlineKeyboardMarkup(buttons);
-                    await bot.SendTextMessageAsync(message.From.Id, "Choose contact", replyMarkup: inline);
+                    Func<UserInfo, bool> func = new Func<UserInfo, bool>((user) => user.UserID == e.Message.From.Id);
+                    int isExist = users.Where(func).Count();
+
+                    if (isExist == 0)
+                        users.Add(new UserInfo(e.Message.From.Id));
+
+                    List<KeyboardButton> howOftenList = new List<KeyboardButton>();
+                    howOftenList.Add(new KeyboardButton(oftenAnswer1));
+                    howOftenList.Add(new KeyboardButton(oftenAnswer2));
+                    howOftenList.Add(new KeyboardButton(oftenAnswer3));
+                    var inline = new ReplyKeyboardMarkup(howOftenList);
+
+                    await bot.SendTextMessageAsync(message.From.Id, e.Message.From.FirstName + ", приветствуем Вас!").
+                        ContinueWith((task) => bot.SendTextMessageAsync(message.From.Id, "Как часто вы ездите отдыхать в год?", replyMarkup: inline));
+                }
+                if (message.Text == oftenAnswer1 || message.Text == oftenAnswer2 || message.Text == oftenAnswer3)
+                {
+                    var currentUser = users.Find(user => user.UserID == e.Message.From.Id);
+                    currentUser.howOftenRest = message.Text;
+
+                    List<KeyboardButton> rests = new List<KeyboardButton>();
+                    rests.Add(new KeyboardButton(restAnswer1));
+                    rests.Add(new KeyboardButton(restAnswer2));
+                    var inline = new ReplyKeyboardMarkup(rests);
+
+                    await bot.SendTextMessageAsync(message.From.Id, "Какой вид отдыха Вы предпочитаете?", replyMarkup: inline);
+                }
+                if (message.Text == restAnswer1 || message.Text == restAnswer2)
+                {
+                    var currentUser = users.Find(user => user.UserID == e.Message.From.Id);
+                    currentUser.restType = message.Text;
+
                 }
             }
         }
---
+++ KavresBot/UserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KavresBot
+{
+    public class UserInfo
+    {
+        private readonly int userID;
+        public string howOftenRest = default;
+        public string restType = default;
+
+        public UserInfo(int id)
+        {
+            this.userID = id;
+        }
+
+        public int UserID { get => userID; }
+    }
+}
